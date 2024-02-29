44498989cd8fbe96964569650647c57cae9e84b3
Parthiv Roshan Menon <parthiv.menon@smarsh.com>
2024-02-29 15:10:12 +0530

    added public/ and private/ route

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 import socket 
 import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/public')
+@app.route('/private')
 def index():
-    return "<h1>Version:2.0 ; I am " + socket.gethostname() + "</h1>"
+    response = {
+        "request.method": request.method,
+        "request.uri":request.url,
+        "headers":json.loads(json.dumps({**request.headers})),
+        "served_by": socket.gethostname()
+    }
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
